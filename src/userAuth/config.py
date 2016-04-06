039617847b882409103c5c8efa7b8bcc20228272
thumbimigwe <thumbimigwe@gmail.com>
2016-04-06 23:40:53 +0300

    config file

--- src/userAuth/config.py
+++ src/userAuth/config.py
@@ -1,2 +1,7 @@
 SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = '77yh66115w1j9n'           #Client ID:
 SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = 'bLkidGQFbacIUIGI'      #Client Secret:
+
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/home/'
+SOCIAL_AUTH_LOGIN_URL = '/'
+
+from config import *
